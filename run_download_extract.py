25fae2dda36bcfd3a41b741da37b5bf3088a9d5d
Ross Maidment <r.i.maidment@reading.ac.uk>
2021-07-20 20:55:45 +0100

    Added additional instructions to run code

--- run_download_extract.py
+++ run_download_extract.py
@@ -1,34 +1,42 @@
 """
 Download and extract TAMSAT rainfall estimates.
 
+Please read the API documentation 'TAMSAT_Data_Download_and_Extraction_API.pdf'
+before using the API.
+
+The code below provides examples to download and extract TAMSAT
+rainfall estimates which you can adapt to your own requirements.
+To run the code, copy and paste the code into your Python console.
+
 Author: TAMSAT (tamsat@reading.ac.uk)
 """
+
 
 # Import the 'download' and 'extract' tools
 from tamsat_download_extract_api import download, extract
 
 
-# Specify local data directory
+# Specify local data directory - this must be changed to a suitable
+# path on your machine
 localdata_dir = '/gws/nopw/j04/tamsat/tamsat/scripts/api/data'
 
-# Download TAMSAT files - copy and paste into terminal to run
+# Download TAMSAT files
 download({
     "timestep": 'daily',
     "resolution": 0.25,
     "start_date": '2021-06-01',
-    "end_date": '2021-06-15',
+    "end_date": '2021-06-30',
     "version": 3.1,
     "localdata_dir": localdata_dir
     })
 
 
-# Extract TAMSAT rainfall estimates at a point - copy and paste into terminal to run
+# Extract TAMSAT rainfall estimates at a point
 extract({
     "extract_type": 'point',
     "longitude": 22.73,
     "latitude": -3.51,
-    "timestep": 'daily',
-    "resolution": 0.25,
+    "timestep": 'pentadal',
     "start_date": '2021-06-01',
     "end_date": '2021-06-15',
     "version": 3.1,
@@ -36,33 +44,31 @@ extract({
     })
     
 
-# Extract area-average TAMSAT rainfall estimates - copy and paste into terminal to run
+# Extract area-average TAMSAT rainfall estimates
 extract({
     "extract_type": 'area_average',
     "N": 22.73,
     "S": 20.73,
     "W": 4.0,
     "E": 5.0,
-    "timestep": 'daily',
-    "resolution": 0.25,
+    "timestep": 'monthly',
     "start_date": '2021-06-01',
-    "end_date": '2021-06-15',
+    "end_date": '2021-06-30',
     "version": 3.1,
     "localdata_dir": localdata_dir
     })
     
 
-# Subset TAMSAT rainfall estimates for a given domain - copy and paste into terminal to run
+# Subset TAMSAT rainfall estimates for a given domain
 extract({
     "extract_type": 'domain',
     "N": 22.73,
     "S": 20.73,
     "W": 4.0,
-    "E": 5.0,
+    "E": 30.0,
     "timestep": 'daily',
-    "resolution": 0.25,
     "start_date": '2021-06-01',
-    "end_date": '2021-06-15',
+    "end_date": '2021-06-30',
     "version": '3.1',
     "localdata_dir": localdata_dir
     })
